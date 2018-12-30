065c5191af78ed87ec0886048b281247988a80e3
snizhanna <44827097+snizhanna@users.noreply.github.com>
2018-12-30 19:51:19 +0200

    Add files via upload

--- proba.js
+++ proba.js
@@ -5,3 +5,5 @@ function myFunction() {
 }
 
 
+<img src="https://picsum.photos/290/290/?1" id="foto1" alt="Описание изображения" title="foto1" style="border: 5px solid rgb(0, 0, 0); margin-left: 5px; margin-top: 5px;">
+<span style="color: white; margin-left: -60px; background: rgba(0, 0, 0, 0.7); padding: 1px;">foto001</span>
